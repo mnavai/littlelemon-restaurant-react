c0eee6f791acea93d08dbf0864c35e52dbc9bbe0
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-01 12:02:21 -0400

    created the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
+import React from "react";
 import Nav from "../component/Nav"
+import Hero from '../component/Hero';
+import Testimonials from '../component/Testimonials';
+import About from '../pages/About';
+import Footer from '../component/Footer';
+
 const Home = () => {
     return (
-        <div className="nav-bar">
+        <div className="home-page">
             <Nav></Nav>
+            <Hero></Hero>
+            <Testimonials></Testimonials>
+            <About></About>
+            <Footer></Footer>
         </div>
-    )
+    );
 }
 export default Nav;
