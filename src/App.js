08d50b6050ad05ad1064f15af10faac09f145ea9
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-01 11:45:58 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
+import ConfirmedBooking from './pages/ConfirmedBooking'
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 //App - starting point
 function App() {
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/Booking' element={<Booking />}></Route>
+        <Route path='/ConfirmedBooking' element={<ConfirmedBooking />}></Route>
+      </Routes>
+      <Footer />
     </>
   );
 }
