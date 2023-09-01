9722aaf3be945d7dc73b9369e046de4b0725fe06
Maryam Navaei <maryam.navaei@gmail.com>
2023-08-31 21:47:48 -0400

    added Nav and NavLink

---
+++ src/component/Footer.jsx
@@ -0,0 +1,35 @@
+import NavLink from "./NavLink"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="logo">
+                <img src="icons_assets/" alt="logo"></img>
+            </div>
+            <div className="doormat-nav">
+                <NavLink>Home</NavLink>
+                <NavLink>About</NavLink>
+                <NavLink>Menu</NavLink>
+                <NavLink>Reservations</NavLink>
+                <NavLink>Order Online</NavLink>
+                <NavLink>Login</NavLink>
+            </div>
+            <div className="contact-info">
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+            <div className="social-media">
+                <ul>
+                    <li>Address</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
