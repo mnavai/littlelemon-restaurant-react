a0b99bfca17ff98b64995114256b54bf8bf41a2b
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-02 14:29:57 -0400

    fixing nav bar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,13 +3,20 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header-section">
-            <div className='text-group'>
-                <h1 className='header-heading'>Little Lemon</h1>
-                <h3 className='header-subheading'>Raleigh</h3>
-                <p className='header-text'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <div className='header-left'>
+                <div className='text-group'>
+                    <h1 className='header-heading'>Little Lemon</h1>
+                    <h3 className='header-subheading'>Raleigh</h3>
+                    <p className='header-text'>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                </div>
+                <div className='btn-group'>
+                    <Button className='btn' type='submit'>Reserve a Table</Button>
+                </div>
             </div>
-            <div className='btn-group'>
-                <Button className='btn' type='submit'>Reserve a Table</Button>
+            <div className='header-right'>
+                <div className='header-img'>
+                    <img src="https://little-lemon-weld.vercel.app/static/media/gourmet.0ff5cb7910f43c4d0a52.jpg" alt="HeadeImage"></img>
+                </div>
             </div>
         </div>
     );
