1747c673eab728612f91231a256d6e80dc295cd8
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-01 21:53:02 -0400

    fixed the issue with displaying the components

--- src/component/Navigation/Nav.jsx
+++ src/component/Navigation/Nav.jsx
@@ -5,9 +5,9 @@ const Nav = () => {
     return (
        <nav className="nav-section">
             {/* <img src="" alt="logo"></img> */}
-            <ul>
-                <li><Link to="/">HOME</Link></li>
-                <li>ABOUT</li>
+            <ul className="links-list">
+                <li className="link-item"><Link to="/">HOME</Link></li>
+                <li className="link-item">ABOUT</li>
                 <li>MENU</li>
                 <li><Link to='/booking'>RESERVATIONS</Link></li>
                 <li>ORDER ONLINE</li>
