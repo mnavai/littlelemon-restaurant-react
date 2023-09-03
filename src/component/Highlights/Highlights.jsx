d05fb494a4cdc74abd503f2ee903b66eb8758e99
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-02 22:36:49 -0400

    fixing footer and highlights

--- src/component/Highlights/Highlights.jsx
+++ src/component/Highlights/Highlights.jsx
@@ -16,12 +16,12 @@ const Highlights = () => {
                       price='$12.99'
                       text='The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.'></Card>
                 <Card className='card'
-                      src=''  
+                      src='https://little-lemon-weld.vercel.app/static/media/bruschetta1.02db8bb7aecd0a390e40.jpg'  
                       title='Bruchetta'
                       price='$5.99'
                       text='Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.'></Card>
                 <Card className='card'
-                      arc='little-lemon-restaurant/icons_assets/lemon dessert.jpg'
+                      src='https://little-lemon-weld.vercel.app/static/media/creme.3a9eaace5be031d2d7a0.jpg'
                       alt='Lemon Desert'  
                       title='Lemon Desert'
                       price='$5.00'
