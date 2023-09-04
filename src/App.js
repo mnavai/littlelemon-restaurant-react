bb92e008af640ef6090a5ec8724f67c1116973d7
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-04 13:16:38 -0400

    adding ConfirmedBooking page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './pages/Home';
 import BookingPage from './pages/BookingPage';
+import ConfirmedBooking from './pages/ConfirmedBooking';
 import NotFound from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -11,6 +12,7 @@ const App =  () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/booking' element={<BookingPage />} />
+        <Route path='/ConfirmedBooking' element={<ConfirmedBooking />}/>
         <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
---
+++ src/pages/ConfirmedBooking.js
@@ -0,0 +1,8 @@
+const ConfirmedBooking = () => {
+    return (
+        <>
+            
+        </>
+    ); 
+}
+export default ConfirmedBooking;
