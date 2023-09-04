3774937391531b6410cb505f33adb575a424e679
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-03 21:36:53 -0400

    booking page implemented

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,10 @@
+import BookingForm from "../component/BookingForm/BookingForm";
+
+const BookingPage = () => {
+    return(
+        <>
+            <BookingForm></BookingForm>
+        </>
+    );
+}
+export default BookingPage;
