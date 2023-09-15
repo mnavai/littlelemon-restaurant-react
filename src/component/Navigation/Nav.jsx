a1d376e2d7fc9f8d750f2d5f405e376bdc11ea54
Maryam Navaei <maryam.navaei@gmail.com>
2023-09-15 15:48:07 -0400

    working on booking confirmation

--- src/component/Navigation/Nav.jsx
+++ src/component/Navigation/Nav.jsx
@@ -3,21 +3,32 @@ import './Nav.css';
 
 const Nav = () => {
     return (
-       <nav className="nav-section">
-            <div className="nav-logo">
-                <img src="https://little-lemon-weld.vercel.app/static/media/logo.7aa41df0270b68cbabda.jpg" alt="logo"></img>
-            </div>
-            <nav className="nav-links">
-                <ul className="links-list">
-                    <li className="link-item"><Link to="/">HOME</Link></li>
-                    <li className="link-item">ABOUT</li>
-                    <li>MENU</li>
-                    <li><Link to='/booking'>RESERVATIONS</Link></li>
-                    <li>ORDER ONLINE</li>
-                    <li>LOGIN</li>
-                </ul>
-            </nav>
-       </nav>
+      <nav className="nav-section">
+        <div className="nav-logo">
+          <img
+            src="https://little-lemon-weld.vercel.app/static/media/logo.7aa41df0270b68cbabda.jpg"
+            alt="logo"
+          ></img>
+        </div>
+        <nav className="nav-links">
+          <ul className="links-list">
+            <li className="link-item">
+              <Link to="/" className="link">
+                HOME
+              </Link>
+            </li>
+            <li className="link-item">ABOUT</li>
+            <li>MENU</li>
+            <li>
+              <Link to="/booking" className="link">
+                RESERVATIONS
+              </Link>
+            </li>
+            <li>ORDER ONLINE</li>
+            <li>LOGIN</li>
+          </ul>
+        </nav>
+      </nav>
     );
 };
 export default Nav;
